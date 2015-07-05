0f7ec0bc4f9f6fe2329fce9f57a990d781f15936
Gregory Plumb <gplumb@wisc.edu>
2015-07-05 09:38:53 -0500

    Changed InitialSetUp.jl to reflect new package name

--- InitialSetUp.jl
+++ InitialSetUp.jl
@@ -1,12 +1,12 @@
-#Installs SnJulia using Julia's package manager and then runs the examples
+#Installs SnFFT using Julia's package manager and then runs the examples
 
 #Open Terminal and change directories to the location of this file
 #Start an interactive julia session
 #Run:
 #	require("InitialSetUp.jl")
 
-Pkg.clone("git://github.com/GDPlumb/SnJulia.jl")
-using SnJulia
+Pkg.clone("git://github.com/GDPlumb/SnFFT.jl")
+using SnFFT
 example1()
 println("")
 example2()
